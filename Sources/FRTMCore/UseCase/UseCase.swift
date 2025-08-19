6102a46a54cad4cfe65367053adf50db9964a981
Valentino Palomba <valentino.palomba@intesasanpaolo.com>
2025-08-19 14:25:45 +0200

    Initial project setup with core module and tests
    Add .gitignore, Swift Package manifest, core source files, and a basic test file. Introduces the UseCase protocol and its extension in the FRTMCore module.

---
+++ Sources/FRTMCore/UseCase/UseCase.swift
@@ -0,0 +1,21 @@
+//
+//  UseCase.swift
+//  FRTMCore
+//
+//  Created by PALOMBA VALENTINO on 19/08/25.
+//
+
+import Foundation
+
+public protocol UseCase<Input, Output> {
+    associatedtype Input
+    associatedtype Output
+    
+    func execute(_ input: Input) throws -> Output
+}
+
+extension UseCase where Input == Void {
+    func execute() throws -> Output {
+        try execute(())
+    }
+}
